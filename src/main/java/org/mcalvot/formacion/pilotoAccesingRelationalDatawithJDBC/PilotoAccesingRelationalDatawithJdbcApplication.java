13519294b6e1e14d8c5e151ee2d395bc0413f9d3
mcalvot <Esma2019*>
2019-03-20 17:49:14 +0100

    first commit

---
+++ src/main/java/org/mcalvot/formacion/pilotoAccesingRelationalDatawithJDBC/PilotoAccesingRelationalDatawithJdbcApplication.java
@@ -0,0 +1,13 @@
+package org.mcalvot.formacion.pilotoAccesingRelationalDatawithJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotoAccesingRelationalDatawithJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotoAccesingRelationalDatawithJdbcApplication.class, args);
+	}
+
+}
